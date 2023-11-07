7a4552c01b6488dd829e249dfb75642b38acacb4
Arthaloith <nguyenmanhhung1904@gmail.com>
2023-11-07 23:37:03 +0700

    added sliding banners component

--- ecommerce/src/components/HeaderComponent/style.js
+++ ecommerce/src/components/HeaderComponent/style.js
@@ -5,6 +5,8 @@ export const WrapperHeader = styled(Row)`
     padding: 10px 120px;
     background-color: rgb(26, 148, 255);
     align-items: center;
+    gap: 16px;
+    flex-wrap: no-wrap;
 `
 
 export const WrapperTextHeader = styled.span`
@@ -24,5 +26,6 @@ export const WrapperHeaderAccount = styled.div`
 export const WrapperTextHeaderSmall = styled.span`
     font-size: 12px;
     color: white;
+    white-space: nowrap;
 `
 
--- ecommerce/src/pages/HomePage/HomePage.jsx
+++ ecommerce/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import TypeProduct from '../../components/TypeProduct/TypeProduct'
 import { WrapperTypeProduct } from './style'
+import SliderComponent from '../../components/SliderComponent/SliderComponent'
+import slider1 from '../../assets/slider1.webp'
+import slider2 from '../../assets/slider2.webp'
+import slider3 from '../../assets/slider3.webp'
 
 const HomePage = () => {
   const arr = ['TV', 'Fridge', 'Laptop']
   return (
-    <div style={{ padding: '0 120px' }}>
+    <>
+      <div style={{ padding: '0 120px' }}>
       <WrapperTypeProduct>
         {arr.map((item) => {
           return (
@@ -13,8 +18,12 @@ const HomePage = () => {
           )
         })}
       </WrapperTypeProduct>
-      HomePage
-    </div>
+      </div>    
+      <div id='container' style={{ backgroundColor: '#efefef', padding: '0 120px'}}>
+          <SliderComponent arrImages={[slider1, slider2, slider3]} />
+      </div>
+    </>
+
   )
 }
 
