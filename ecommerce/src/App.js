f6103c2b1acea4dd6ae08a3f98a5f80b6ef09b03
Arthaloith <nguyenmanhhung1904@gmail.com>
2023-11-07 10:10:26 +0700

    added routing and stuffs

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import OrderPage from './pages/OrderPage/OrderPage'
+import { routes } from './routes'
+import DefaultComponent from './components/DefaultComponent/DefaultComponent'
 function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path ='/' element={<HomePage />}></Route>
-          <Route path ='/order' element={<OrderPage />}></Route>
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
+            {routes.map((route) => {
+              const Page = route.page
+              const Layout = route.isShowHeader ? DefaultComponent : React.Fragment
+              return (
+                <Route key={route.path} path={route.path} element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                } />
+              )
+            })}
+          </Routes>
+        </Router>
     </div>
   )
 }
