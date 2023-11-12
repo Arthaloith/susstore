e6bff2c081ad246eb629fe69653ea1c57e34d6f7
Arthaloith <nguyenmanhhung1904@gmail.com>
2023-11-12 18:38:56 +0700

    added token expiration

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -8,26 +8,39 @@ import { updateUser } from './redux/slides/userSlide'
 import { jwtDecode } from 'jwt-decode'
 import axios from 'axios'
 import { isJsonString } from './utils'
+
 function App() {
   const dispatch = useDispatch();
 
-  axios.interceptors.request.use(async (config) => {
+  useEffect(() => {
+    const { storageData, decoded } = handleDecoded()
+    if (decoded?.id) {
+      handleGetDetailsUser(decoded?.id, storageData)
+    }
+  }, [])
+
+  const handleDecoded = () => {
+    let storageData = localStorage.getItem('access_token')
+    let decoded = {}
+    if (storageData && isJsonString(storageData)) {
+      storageData = JSON.parse(storageData)
+      decoded = jwtDecode(storageData)
+    }
+    return { decoded, storageData }
+  }
+
+  UserService.axiosJWT.interceptors.request.use(async (config) => {
     // Do something before request is sent
+    const currentTime = new Date()
+    const { decoded } = handleDecoded()
+    if (decoded?.exp < currentTime.getTime() / 1000) {
+      const data = await UserService.refreshToken()
+      config.headers['token'] = `Bearer ${data?.access_token}`
+    }
     return config;
   }, (err) => {
     return Promise.reject(err)
   })
-
-  useEffect(() => {
-    let storageData = localStorage.getItem('access_token')
-    if (storageData && isJsonString(storageData)) {
-      storageData = JSON.parse(storageData)
-      const decoded = jwtDecode(storageData)
-      if (decoded?.id) {
-        handleGetDetailsUser(decoded?.id, storageData)
-      }
-    }
-  }, [])
 
   const handleGetDetailsUser = async (id, token) => {
     const res = await UserService.getDetailsUser(id, token)
